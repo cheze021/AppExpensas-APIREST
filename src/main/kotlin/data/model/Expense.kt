2d49d76d62fe8594b25e8d6de3b01a0bc65a23cc
Ezequiel Introini <chezemanya@gmail.com>
2025-05-03 16:38:51 -0300

    Expenses New Model: - Se modificaron los nombres de las categorias

--- src/main/kotlin/data/model/Expense.kt
+++ src/main/kotlin/data/model/Expense.kt
@@ -3,23 +3,23 @@ package com.example.data.model
 import kotlinx.serialization.Serializable
 
 val expenses = mutableListOf(
-    Expense(id = 1, 70.0, "GROCERIES", "Weekly buy", "2024-12-10"),
-    Expense(id = 2, 45.5, "PARTY", "Birthday party", "2024-12-10"),
-    Expense(id = 3, 12.0, "SNACKS", "Afternoon snack", "2024-12-10"),
-    Expense(id = 4, 5.5, "COFFEE", "Morning coffee", "2024-12-10"),
-    Expense(id = 5, 150.0, "CAR", "Gas refill", "2024-12-10"),
-    Expense(id = 6, 1200.0, "HOUSE", "Rent payment", "2024-12-10"),
-    Expense(id = 7, 30.0, "OTHER", "Random purchase", "2024-12-10")
+    Expense(id = 1, 1000.00, "LUZ", "UTE", "2024-12-10"),
+    Expense(id = 2, 750.00, "INTERNET", "Antel", "2024-12-10"),
+    Expense(id = 3, 500.00, "SUPER", "Surtidos", "2024-12-10"),
+    Expense(id = 4, 13650.00, "ALQUILER", "Alquiler apto", "2024-12-10"),
+    Expense(id = 5, 350.00, "MERIENDAS", "Merienda en ....", "2024-12-10"),
+    Expense(id = 6, 750.00, "PEDIDOS_YA", "Deliverys de comida", "2024-12-10"),
+    Expense(id = 7, 350.00, "OTROS", "Compra random", "2024-12-10")
 )
 
 val expensesCategories: List<String> = mutableListOf(
-    "GROCERIES",
-    "PARTY",
-    "SNACKS",
-    "COFFEE",
-    "CAR",
-    "HOUSE",
-    "OTHER",
+    "LUZ",
+    "INTERNET",
+    "SUPER",
+    "ALQUILER",
+    "MERIENDAS",
+    "PEDIDOS_YA",
+    "OTROS",
 )
 
 @Serializable
