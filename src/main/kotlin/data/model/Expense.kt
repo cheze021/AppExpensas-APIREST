372ca40fa5a6c1a57679b3a4fd9530848877856e
Ezequiel Introini <chezemanya@gmail.com>
2025-05-03 15:14:54 -0300

    Expenses New Model: - Se adaptó las Expensas a recibir parametro DATE por el front - Agregadas DATE hardcoded a la lista de expensas por defecto

--- src/main/kotlin/data/model/Expense.kt
+++ src/main/kotlin/data/model/Expense.kt
@@ -3,13 +3,13 @@ package com.example.data.model
 import kotlinx.serialization.Serializable
 
 val expenses = mutableListOf(
-    Expense(id = 1, 70.0, "GROCERIES", "Weekly buy"),
-    Expense(id = 2, 45.5, "PARTY", "Birthday party"),
-    Expense(id = 3, 12.0, "SNACKS", "Afternoon snack"),
-    Expense(id = 4, 5.5, "COFFEE", "Morning coffee"),
-    Expense(id = 5, 150.0, "CAR", "Gas refill"),
-    Expense(id = 6, 1200.0, "HOUSE", "Rent payment"),
-    Expense(id = 7, 30.0, "OTHER", "Random purchase")
+    Expense(id = 1, 70.0, "GROCERIES", "Weekly buy", "2024-12-10"),
+    Expense(id = 2, 45.5, "PARTY", "Birthday party", "2024-12-10"),
+    Expense(id = 3, 12.0, "SNACKS", "Afternoon snack", "2024-12-10"),
+    Expense(id = 4, 5.5, "COFFEE", "Morning coffee", "2024-12-10"),
+    Expense(id = 5, 150.0, "CAR", "Gas refill", "2024-12-10"),
+    Expense(id = 6, 1200.0, "HOUSE", "Rent payment", "2024-12-10"),
+    Expense(id = 7, 30.0, "OTHER", "Random purchase", "2024-12-10")
 )
 
 val expensesCategories: List<String> = mutableListOf(
@@ -27,5 +27,6 @@ data class Expense (
     val id: Long = -1,
     val amount: Double,
     val categoryName: String,
-    val description: String
+    val description: String,
+    val date: String
 )
